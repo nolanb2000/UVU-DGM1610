2b6cf8c61c3d7ae807f9d4afce6360213f6a1637
NOLAN <theonlyarmydog@gmail.com>
2018-09-24 17:20:15 -0600

    Added Double Jump Worked on score

--- DGM1610_firstgame/Assets/stripts/CharactorMove.cs
+++ DGM1610_firstgame/Assets/stripts/CharactorMove.cs
@@ -7,12 +7,16 @@ public class CharactorMove : MonoBehaviour {
 	//Player Movment Variables 
 	public int MoveSpeed;
 	public float JumpHeight;
+	private bool DoubleJump;
 
 	//player grounded variables 
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
 	private bool Grounded;
+
+	//Non-Stick Player
+	private float MoveVelocity;
 
 	// Use this for initialization
 	void Start () {
@@ -29,15 +33,36 @@ public class CharactorMove : MonoBehaviour {
 		//This Code makes the character jump
 		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 			Jump();
+
 		}
+
+		//Double Jump
+		if(Grounded)
+			DoubleJump = false;
+
+		if(Input.GetKeyDown(KeyCode.Space)&& !DoubleJump && !Grounded){
+			Jump();
+			DoubleJump = true;
+		}
+		
+		//Non-Stick Player
+		MoveVelocity = 0f;
+	
 
 		//This lets your character move from side to side 
 		if(Input.GetKey (KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = MoveSpeed;
 		}
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = -MoveSpeed;
 		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+
+
 	}
 
 	public void Jump(){
---
+++ DGM1610_firstgame/Assets/stripts/Score Holder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public static int Score;
+
+	Text ScoreText;
+
+public class ScoreHolder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ScoreText = GetComponent<Text>();
+
+		Score = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Score < 0)
+			Score = 0;
+		ScoreText.Text = "" + Score;
+	}
+
+	public static void AddPoints (int PointsToAdd){
+		Score += PointsToAdd;
+	}
+
+
+
+		
+	
+}

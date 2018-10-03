1066d23e4799ff700b42e9bc5b5e6a11afbab257
NOLAN <theonlyarmydog@gmail.com>
2018-10-03 17:14:53 -0600

    Added level manager and pop quiz

---
+++ DGM1610_firstgame/Assets/stripts/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject CurrentCheckPoint;
+	private Rigidbody2D Player; 
+
+	//Particles
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle 
+
+	//Respawn Delay
+	public float RespawnDelay;
+
+	//Penalty on Death 
+	public int PointPenaltyOnDeath;
+
+	//Store Gravity Value 
+	private float GravityStore;
+
+	// Use this for initialization
+	void Start () {
+		Player = FindObjectOfType<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RespawnPlayer(){
+		StartCoroutine("RespawnPlayerCo")
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		//Makes deathparticle 
+		Instantiate (DeathParticle, Player.Transform.Position, Player.Transform.Rotation);
+		//Hide player
+		Player.enabled = false;
+		player.GetComponent<Renderer> ().enabled = false;
+		//Gravity Reset 
+		GravityStore = Player.GetComponent<Rigidbody2D>().GravityScale;
+		Player.GetComponent<Rigidbody2D().GravityScale = 0f; 
+		Player.GetComponent<Rigidbody2D>().Velocity = Vector2.zero;
+		//Score Penalty
+		ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		//Debug Message 
+		Debug.Log("Player Respawn");
+	}
+}
---
+++ DGM1610_firstgame/PopQuiz.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopQuiz : MonoBehaviour {
+
+
+//Int Holder 
+//and define public or private
+public int Variable1;
+public int Variable2; 
+public int Total;
+
+
+void Start(){
+	CalcAdd();
+}
+//Didnt Add this part 
+public void Calc(){
+
+	Add(Variable1,Variable2)
+ }
+
+//Adds the two Variables and prints it 
+ public void Add( int Variable1 , int Variable2){
+	//Tells what Total is 
+	Total = Variable1 + Variable2;
+	//prints the Total 
+	Print (Total); 
+	
+}
+
+
+}

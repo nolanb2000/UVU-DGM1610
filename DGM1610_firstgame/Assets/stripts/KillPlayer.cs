a78f23d37710f017064b52d8d52e7f14294e9921
NOLAN <theonlyarmydog@gmail.com>
2018-10-10 17:19:06 -0600

    Added kill player scrtipt

---
+++ DGM1610_firstgame/Assets/stripts/KillPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour {
+
+	public LevelManager LevelManager;
+
+	// Use this for initialization
+	void Start () {
+		LevelManager = FindObjectOfType <LevelManager>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.name =="Player"){
+			LevelManager.RespawnPlayer();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

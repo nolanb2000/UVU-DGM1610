70db3757eadb926d344097e069e520d27eb8d2b7
NOLAN <theonlyarmydog@gmail.com>
2018-10-15 17:22:15 -0600

    Added enemy and Wall

---
+++ DGM1610_firstgame/Assets/stripts/EnemyPatrol.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyPatrol : MonoBehaviour {
+
+	//movment vairables 
+	public float MoveSpeed;
+	public bool MoveRight;
+
+    //Wall Check 
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	public bool HittingWall;
+
+    //Edge Check 
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+	if (HittingWall || !NotAtEdge){
+		MoveRight = !MoveRight;
+	}
+
+	if (MoveRight){
+		transform.localScale = new Vector3(-7.101518f,7.9380f,148646f);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+	else {
+		transform.localScale = new Vector3(7.101518f,7.9380f,148646f);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+    }
+ 
+}
+
+
---
+++ DGM1610_firstgame/Homework and quizes/IfandElse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfandElse : MonoBehaviour {
+
+
+	public string Candy; 
+
+	// Use this for initialization
+	void Start () {
+	
+		if (Candy == "Choclate");
+			print("Give Them Sneakers");
+		else if (Candy == "Gummy")
+			print("Give Them GummyBears");
+		else if (Candy == "Sweet")
+			print("Give Them Lolipop");
+		else 
+			print ("They get Nothing!");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

cd98168d7a403ee14f7fad98436683051cce124e
NOLAN <theonlyarmydog@gmail.com>
2018-10-22 17:17:34 -0600

    Addded Shooting System

---
+++ DGM1610_firstgame/Assets/stripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float Speed;
+	public Rigidbody2D Player;
+
+	public GameObject EnemyDeath;
+
+	public GameObject ProjectileParticle;
+
+	public int PointsForKill;
+
+	// Use this for initialization
+	void Start () {
+
+		if(Player.transform.localScale.x < 0)
+			Speed =-Speed;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag =="Enemy"){
+			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+			ScoreHolder.AddPoints(PointsForKill);
+		}
+
+		Instantiate(ProjectileParticle, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}

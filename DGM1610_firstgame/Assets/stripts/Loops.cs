05e3864faa8e4f48b2c8d94b4d3d4ee418a69788
NOLAN <theonlyarmydog@gmail.com>
2018-10-24 17:04:01 -0600

    Practiced Loops For and While

---
+++ DGM1610_firstgame/Assets/stripts/Loops.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	// public int Num = 40;
+
+	// public string Color = "Blue";
+
+	// Use this for initialization
+	void Start () {
+		// for(int i = 0; i <= 100; i++){
+
+		// 	print(i +"Ants Marching");
+
+		// }
+
+		// while(Num >= 0){
+		// 	print("Countdown" + Num);
+		// 	Num --;
+		// }
+
+		// while( Color == "Blue" ){
+		// 	print("Color is" + Color); 
+		// }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

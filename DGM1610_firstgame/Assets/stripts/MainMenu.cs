e2c78d982c18f8e6b2136950a4173de53afe08a1
NOLAN <theonlyarmydog@gmail.com>
2018-11-26 17:13:29 -0700

    Added main Menu

---
+++ DGM1610_firstgame/Assets/stripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	public int LevelToLoad;
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(LevelToLoad);
+	}
+
+	public void LevelExit(){
+		Application.Quit();
+	}
+}

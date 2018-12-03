e8f903805be05b5ff684358186ec554c7d8fb999
NOLAN <theonlyarmydog@gmail.com>
2018-12-03 11:50:26 -0700

    Worked on Hw Array

--- DGM1610_firstgame/Homework/Array.cs
+++ DGM1610_firstgame/Homework/Array.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Array : MonoBehaviour {
 	
 	public string[] Animals; 
+	public string[] Weapons;
+	public string[] Monsters;
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +22,33 @@ public class Array : MonoBehaviour {
 		foreach(string item in Animals){
 			print("I Want a " + item);
 		}
+
+
+		Weapons = new string[5];
+
+		Weapons[0] ="Sword";
+		Weapons[1] ="Bow";
+		Weapons[2] ="Axe";
+		Weapons[3] ="Dagger";
+		Weapons[4] ="GreatSword";
+
+		foreach(string item in Weapons){
+			print("Would you like tom buy a " + item);
+		}
+
+		Monsters = new string[5];
+
+		Monsters[0] ="Werewolf";
+		Monsters[1] ="Dragons";
+		Monsters[2] ="Wraith";
+		Monsters[3] ="Goblin";
+		Monsters[4] ="Wyvern";
+
+		foreach(string item in Monsters){
+			print("You must Hunt Down a " + item);
+		}
+
+
 	}
 	
 	// Update is called once per frame
